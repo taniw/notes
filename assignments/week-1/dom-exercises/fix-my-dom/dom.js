5b3899c8f1a3b5738a3e6ae6cb6b7f19b7728e04
Tani Wong <tani.s.wong@gmail.com>
2014-11-20 16:48:07 -0800

    Dom exercises

--- assignments/week-1/dom-exercises/fix-my-dom/dom.js
+++ assignments/week-1/dom-exercises/fix-my-dom/dom.js
@@ -1,1 +1,3 @@
-// write code here
+document.querySelector("li").textContent="first";
+document.querySelectorAll("li")[1].textContent = "second";
+document.querySelectorAll("li")[3].textContent = "fourth";
